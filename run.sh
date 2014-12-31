1496bc1ab57b7a902c98d43dc9584fcd5123c171
Arshak Navruzyan <arshakn@gmail.com>
2014-12-31 12:29:13 -0800

    modified run script to have an arg

--- run.sh
+++ run.sh
@@ -1,3 +1,13 @@
-docker run -d -p 8998:8998 julia
-docker run -d -p 8999:8999 theano
-docker run -it --rm vowpal_wabbit vw
+case "$1" in
+  julia)
+    docker run -d -p 8998:8998 julia
+    sleep 2 && open http://localhost:8998
+  ;;
+  theano)
+    docker run -d -p 8999:8999 theano
+    open http://localhost:8999
+  ;;
+  vw)
+    docker run -it --rm vowpal_wabbit vw
+  ;;
+esac
