ff96ef848badb084feefbd8dd42ce8d80a9db1ac
MJK <mjk@emmjaykay.org>
2014-12-31 21:46:31 -0800

    some additional things for docker on ubuntu

--- build.sh
+++ build.sh
@@ -1,13 +1,34 @@
 #!/usr/bin/env bash
 
-#nat 8000-9000 tcp/udp ports from virtualbox
-if [ ! -f ".nat" ]; then 
-  for i in {8000..9000}; do
-    VBoxManage controlvm "boot2docker-vm" natpf1 "tcp-port$i,tcp,,$i,,$i";
-    VBoxManage controlvm "boot2docker-vm" natpf1 "udp-port$i,udp,,$i,,$i";
-    touch .nat
-  done
-fi 
+function mac_build_prerequisite {
+    #nat 8000-9000 tcp/udp ports from virtualbox
+    if [ ! -f ".nat" ]; then 
+	for i in {8000..9000}; do
+	    VBoxManage controlvm "boot2docker-vm" natpf1 "tcp-port$i,tcp,,$i,,$i";
+	    VBoxManage controlvm "boot2docker-vm" natpf1 "udp-port$i,udp,,$i,,$i";
+	    touch .nat
+	done
+    fi 
+}
+
+function get_host {
+    local __resultvar=$1
+    #local myresult="$(expr substr $(uname -s) 1 5)"
+    local myresult="$(uname -s)"
+    eval $__resultvar="'$myresult'"
+}
+
+if [ "$(uname)" == "Darwin"]; then
+    echo -n "Preparing to build Macintosh prerequisites..."    
+    mac_build_prerequisite
+    echo "done!"
+elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
+    echo -n "Preparing to build Linux prerequisites..."
+    echo "done!"
+elif [ "$(expr substr $(uname -s) 1 10)" == "MINGW32_NT" ]; then
+    echo -n "Preparing to build Windows prerequisites..."
+    echo "done!"
+fi
 
 #build only argument otherwise all directories
 if [ -z "$1" ]; then
@@ -21,9 +42,15 @@ function error_exit {
 	exit 1
 }
 
+get_host result
 #must run from boot2docker
-if [ -z DOCKER_HOST ]; then
+if [ -z DOCKER_HOST ] && [ $result="Darwin" ]; then
   error_exit "not in boot2docker"
+fi
+
+if [ "$(id -u)" -ne 0 ] && [ $result="Linux"  ]; then
+    echo  "We need a sudoer password to add you to the docker group."
+    sudo usermod -a -G docker $USER
 fi
 
 echo "Starting builds"
