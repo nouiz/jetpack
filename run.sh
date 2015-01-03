e7273a6bea95e21311e5825dde2609e1e35219b8
Arshak Navruzyan <arshakn@gmail.com>
2015-01-02 23:32:23 -0800

    added h2o renamed spark to mllib

--- run.sh
+++ run.sh
@@ -1,4 +1,12 @@
 case "$1" in
+  graphlab)
+    docker run -d -p 8997:8997 graphlab
+    sleep 2 && open http://localhost:8997
+  ;;
+  h2o)
+    docker run -d -p 8996:8996 h2o
+    sleep 2 && open http://localhost8996
+  ;;
   julia)
     docker run -d -p 8998:8998 julia
     sleep 2 && open http://localhost:8998
@@ -6,10 +14,6 @@ case "$1" in
   theano)
     docker run -d -p 8999:8999 theano
     sleep 2 && open http://localhost:8999
-  ;;
-  graphlab)
-    docker run -d -p 8997:8997 graphlab
-    sleep 2 && open http://localhost:8997
   ;;
   vw)
     docker run -it --rm vowpal_wabbit vw
