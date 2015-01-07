67b383792a4bfc15ceb932918956e5a66f9954c2
MJK <mjk@emmjaykay.org>
2015-01-06 23:46:36 -0800

    add help menu to run.sh

--- run.sh
+++ run.sh
@@ -1,3 +1,24 @@
+
+
+usage="$(basename "$0") [package] -- run the selected package and have it connect to the outside
+
+where:
+    package - one of
+                graphlab
+                h2o
+                julia
+                theano
+                vw
+                mllib
+"
+
+if [ $# -eq 0 ]
+  then
+    echo "No arguments supplied"
+    echo "$usage"
+    exit 1
+fi
+
 case "$1" in
   graphlab)
     docker run -d -p 8997:8997 graphlab
