b1cf6c7f215d08aec1575ab15b17ad485eaa79db
agibsonccc <0@blix.io>
2015-01-15 20:51:44 -0800

    change run.sh run the demo

--- deeplearning4j/inst/run.sh
+++ deeplearning4j/inst/run.sh
@@ -1,9 +1,2 @@
-
-rm -rf /data/mapred/*
-mkdir -p /data/mapred
-chmod 777 /data/mapred
-
-/etc/init.d/hadoop-0.20-mapreduce-tasktracker start
-
-sleep 10
-tail -F /var/log/hadoop-0.20-mapreduce/hadoop-hadoop-tasktracker*.out
+#!/bin/bash
+java -cp "hadoop-lib/lib/*" org.deeplearning4j.App /hadoop-example-native/src/main/resources/conf/ir.properties
